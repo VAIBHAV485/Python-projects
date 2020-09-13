d59a344a60dfafa88156b5727c68b097001caa59
VAIBHAV485 <62990661+VAIBHAV485@users.noreply.github.com>
2020-09-13 14:42:25 +0530

    Add files via upload

---
+++ login.py
@@ -0,0 +1,142 @@
+from tkinter import *
+import os
+
+
+def register_user(): 
+    username_info = username.get()
+    password_info = password.get()
+
+    file = open(username_info, "a")
+    file.write(username_info)
+    file.write('\n')
+    file.write(password_info)
+    file.close()
+
+    username_entry.delete(0, END)
+    password_entry.delete(0, END)
+
+    Label(screen1, text="Registration successful", fg="green").pack()
+
+
+def register_screen():
+    global screen1
+    screen1 = Toplevel(screen)
+    screen1.title("Register")
+    screen1.geometry("300x250")
+
+    global username
+    global password
+    global username_entry
+    global password_entry
+    username = StringVar()
+    password = StringVar()
+
+    Label(screen1, text="Enter the details below").pack()
+    Label(screen1, text="").pack()
+    Label(screen1, text="Username *").pack()
+    username_entry = Entry(screen1, textvariable=username)
+    username_entry.pack()
+    Label(screen1, text="Password *").pack()
+    password_entry = Entry(screen1, textvariable=password)
+    password_entry.pack()
+    Button(screen1, text="Register", width="10", height="1", command=register_user).pack()
+
+
+def login():
+    global screen2
+    screen2 = Toplevel(screen)
+    screen2.title("Login")
+    screen2.geometry("300x250")
+    Label(screen2, text="Enter the details below to login").pack()
+    Label(screen2, text="").pack()
+
+    global username_verify
+    global password_verify
+    username_verify = StringVar()
+    password_verify = StringVar()
+
+    global username_entry1
+    global password_entry1
+    Label(screen2, text="Username *").pack()
+    username_entry1 = Entry(screen2, textvariable=username_verify)
+    username_entry1.pack()
+    Label(screen2, text="").pack()
+    Label(screen2, text="Password*").pack()
+    password_entry1 = Entry(screen2, textvariable=password_verify)
+    password_entry1.pack()
+    Label(screen2, text="").pack()
+    Button(screen2, text="Login", width="10", height="1", command=login_verify).pack()
+
+    print("Login session started")
+
+
+def login_verify():
+    username1 = username_verify.get()
+    password1 = password_verify.get()
+    username_entry1.delete(0, END)
+    password_entry1.delete(0, END)
+
+    list_of_files = os.listdir()
+    if username1 in list_of_files:
+        file1 = open(username1, "r")
+        verify = file1.read().splitlines()
+        if password1 in verify:
+            login_success()
+        else:
+            wrong_password()
+    else:
+        user_not_found()
+
+def login_success():
+    global screen3
+    screen3 = Toplevel(screen)
+    screen3.title("Success")
+    screen3.geometry("150x100")
+    Label(screen3, text="Login Success", fg="green", font=("Calibri", 15)).pack()
+    Button(screen3, text="OK", command=delete2).pack()
+
+
+def delete2():
+    screen3.destroy()
+
+
+def wrong_password():
+    global screen4
+    screen4 = Toplevel(screen)
+    screen4.title("Success")
+    screen4.geometry("150x100")
+    Label(screen4, text="Wrong password", fg="green", font=("Calibri", 15)).pack()
+    Button(screen4, text="OK", command=delete3).pack()
+
+
+def delete3():
+    screen4.destroy()
+
+
+def user_not_found():
+    global screen5
+    screen5 = Toplevel(screen)
+    screen5.title("Success")
+    screen5.geometry("150x100")
+    Label(screen5, text="User not found", fg="green", font=("Calibri", 15)).pack()
+    Button(screen5, text="OK", command=delete4).pack()
+
+
+def delete4():
+    screen5.destroy()
+
+
+def main_screen():
+    global screen
+    screen = Tk()
+    screen.geometry("300x250")
+    screen.title("Login System")
+    Label(text="Welcome", bg="blue", width="30", height="2", font=("Calibri", 13)).pack()
+    Label(text="").pack()
+    Button(text="Login", width="30", height="2", command=login).pack()
+    Label(text="").pack()
+    Button(text="Register", width="30", height="2", command=register_screen).pack()
+    screen.mainloop()
+
+
+main_screen()
